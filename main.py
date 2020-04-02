c2289b4f3c7bed2fe0a1424cd6091cc5f50723b9
brodiewindle <53396361+brodiewindle@users.noreply.github.com>
2020-04-03 09:42:53 +1000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+This program is designed to suggest trades for short, medium and long term. It is only a guide and all
+suggestions must be taken and further investigated before trades are made
+"""
+
+# Import Statements
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+
+__author__ = 'Brodie Windle'
+
